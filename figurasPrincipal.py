0a7c436e03b5317c846c460108d0894144e40599
Regina <a01662965@tec.mx>
2025-11-02 18:49:46 -0600

    Añadí las funciones de perímetro y área para el resultado final

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from funciones import a_triangulo, p_triangulo, a_rectangulo, p_rectangulo, a_circulo, p_circulo 
+
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -7,5 +9,14 @@ print("Procesando figuras ...\n")
 areas = []
 perimetros = []
 
-for index, row in data.File.iterrows():
-        print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, MEDIDA2={row['MEDIDA2']}")
+for index, row in dataFile.iterrows():
+   if row['FIGURA'] == 't':
+      area = a_triangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+      perimetro = p_triangulo(int(row['MEDIDA1']))
+   elif row["FIGURA"] == 'r':
+      area = a_rectangulo(int(row["MEDIDA1']), int(row['MEDIDA2']))
+      perimetro = p_rectangulo(int(row['MEDIDA1']), int(row['MEDIDA2]))
+   elif row ['FIGURA'] == 'c':
+      area = round(a_circulo(float(row['MEDIDA1'])),2)
+      perimetro = round(p_circulo(float(row['MEDIDA1'])),2)
+print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1]}, Medida2={row['MEDIDA2']}, Area={area}, Perímetro={perimetro}")
---
+++ funciones.py
@@ -0,0 +1,23 @@
+def a_rectangulo(x,y):
+        area=int(x*y)
+        return area
+
+def p_rectangulo(x,y):
+        perimetro=int((x*2)+(y*2))
+        return perimetro
+
+def a_triangulo(x,y):
+        area=float((x*y)/2)
+        return area
+
+def p_triangulo(x):
+        perimetro=int(x*3)
+        return perimetro
+
+def a_circulo(x): 
+        area=float(3.1416*(x**2))
+        return area
+
+def p_circulo(x):
+        perimetro=float(3.1416*(x*2))
+        return perimetro
